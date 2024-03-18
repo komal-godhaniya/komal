54b2704c1ec4259e05e7ceecfad3fdde1f5b4be5
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-03-18 17:56:21 +0530

    Add files via upload

---
+++ looping-2/task3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+    int i=0,num;
+
+    printf("enter number:");
+    scanf("%d",&num);
+
+    do
+    {
+        printf("%d\n",i);
+        i+=2;
+    } while (i<=num);
+    
+}
