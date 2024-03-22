9691da58812fbdd574957e743da4099909cd3de1
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-03-22 17:51:44 +0530

    Add files via upload

---
+++ pattern-2/ptn7.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main(){
+
+    //   1 2 3 4 5
+    //   1 2 3 4
+    //   1 2 3
+    //   1 2
+    //   1
+    //   1 2
+    //   1 2 3
+    //   1 2 3 4
+    //   1 2 3 4 5
+
+
+    for (int i = 5; i >=1 ; i--){
+
+        for(int j =1 ; j<=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+     for (int i = 1 ; i <=5 ; i++){
+
+        for(int j =1 ; j<=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }    
+
+}
