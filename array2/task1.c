ad24c077649846955365bdcf9e6519956c07329a
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-04-01 16:55:36 +0530

    Add files via upload

---
+++ array2/task1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ main() {
+    float avr;
+   int arr[3][3],sum;
+ 
+   printf("Enter array's elements:\n");
+
+   for(int i=0; i<=2; i++)
+   {
+       for(int j=0; j<=2; j++)
+       {
+           printf("enter value:");
+           scanf("%d",&arr[i][j]);
+       }
+       printf("\n");
+   }
+   for(int a=0; a<3; a++){
+       for(int b=0; b<3; b++){
+           printf("%d",arr[a][b]);
+           sum+=arr[a][b];
+       }
+       printf("\n");
+   }
+   printf("%d\n",sum);
+   avr=sum/9;
+   printf("%0.2f",avr);
+}
