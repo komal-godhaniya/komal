1d16977e34b265d540f7fb35d4d98ddfb439a8f0
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-04-04 14:49:07 +0530

    Add files via upload

---
+++ string-2/task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  char name[]="KOMAL";
+   
+  for(int i=0;name[i]!= NULL; i++)
+  {
+      if(name[i]>='A' && name[i]<='Z'){
+          name[i]+=32;
+      }
+      printf("%c",name[i]);
+  }
+   
+}
