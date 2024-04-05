ce36e79744524e4a75e76f8d214c27e735a31210
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-04-05 16:16:46 +0530

    Add files via upload

--- string-s-1/task1.c
+++ string-s-1/task1.c
@@ -4,47 +4,56 @@ main(){
 	
 	//	task-1
 	
-	char k[100];
-	printf("Enter Your Name:");
-	gets(k);
-	puts(strupr(k));
-
-	printf("\n\n");
+//	char k[100];
+//	printf("Enter Your Name:");
+//	gets(k);
+//	puts(strupr(k));
+//
+//	printf("\n\n");
 
 	//	task-2
 	
-	char o[100];
-	printf("Enter Your Name:");
-	gets(o);
-	puts(strlwr(o));
-	
-	printf("\n\n");
+//	char o[100];
+//	printf("Enter Your Name:");
+//	gets(o);
+//	puts(strlwr(o));
+//	
+//	printf("\n\n");
 
 	//	task-3
 	
-	char m[100];
-	printf("Enter Your Name:");
-	gets(m);
-	printf("%d",strlen(m));
-
-	printf("\n\n");
+//	char m[100];
+//	printf("Enter Your Name:");
+//	gets(m);
+//	printf("%d",strlen(m));
+//
+//	printf("\n\n");
 
 
 	//	task-4
 	
-	char a[100];
-	printf("Enter Your Name:");
-	gets(a);
-	puts(a);
-
-	printf("\n\n");
+//	char a[100];
+//	printf("Enter Your Name:");
+//	gets(a);
+//	puts(a);
+//
+//	printf("\n\n");
 
 
    	//	task-5
    	
-   	char firstname[] = "Komal";
-   	char lastname[] = "Godhaniya";
+   	char firstname[100];
+   	char lastname[100];
    	char fullname[100];
+   	
+   	printf("Enter firstname:");
+	gets(firstname);
+	puts(firstname);
+	
+	
+	printf("Enter lastname:");
+	gets(lastname);
+	puts(lastname);
    	
    	strcpy(fullname, strcat(firstname , lastname));
    	
@@ -54,23 +63,23 @@ main(){
 
 	//	task-6
 
-	char g[100],n[100];
-	printf("Enter value-1:");
-	gets(g);
-	puts(g);
-	
-	
-	printf("Enter value-1:");
-	gets(n);
-	puts(n);
-	
-
-
-	if(strcmp(g,k) == 0){
-		printf("Strings are equal");
-	}
-	else{
-		printf("No, they are not equal");
-    }
-		
+//	char g[100],n[100];
+//	printf("Enter value-1:");
+//	gets(g);
+//	puts(g);
+//	
+//	
+//	printf("Enter value-1:");
+//	gets(n);
+//	puts(n);
+//	
+//
+//
+//	if(strcmp(g,k) == 0){
+//		printf("Strings are equal");
+//	}
+//	else{
+//		printf("No, they are not equal");
+//    }
+//		
 }
